21fbf07a66efdedbcce7dd551f34310b32792014
Ankit Tehlan <ankitehlan@gmail.com>
2023-10-17 00:49:14 +0530

    Add route to get transaction list

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 # Import libraries
+from flask import Flask, request, url_for, redirect, render_template
 
 # Instantiate Flask functionality
+app = Flask(__name__)
 
 # Sample data
+transactions = [
+    {'id': 1, 'date': '2023-06-01', 'amount': 100},
+    {'id': 2, 'date': '2023-06-02', 'amount': -200},
+    {'id': 3, 'date': '2023-06-03', 'amount': 300}
+]
 
 # Read operation
+@app.route("/")
+def get_transactions():
+    return {"hello":"ads"}
 
 # Create operation
 
@@ -13,4 +23,6 @@
 # Delete operation
 
 # Run the Flask app
+if __name__ == "__main__":
+    app.run(debug=True)
     
